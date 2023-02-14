c4413e136a9a76921d65800948ea779c093b514c
NotoRen <s.noto.2282@vallauri.edu>
2023-02-14 09:16:06 +0100

    update

---
+++ CarShopLibrary/Moto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShopLibrary
+{
+    public enum ETipoMoto
+    {
+        Undefined,
+        Cross,
+        Enduro,
+        Strada,
+        Chopper,
+        Touring
+    }
+    public class Moto:Veicolo
+    {
+        public ETipoMoto Tipo { get; set; }
+        public int NumTempi { get; set; }
+        public Moto(string marca, string modello) : base(marca, modello) { }
+        public Moto(string marca, string modello,ETipoMoto tipo,int numTempi) : base(marca, modello)
+        {
+            Tipo = tipo;
+            NumTempi = numTempi;
+        }
+        public Moto(string marca, string modello, string vin, string colore, int Km, EAlimentazione alimentazione,
+            int MaxSpeed, int potenza, DateTime Data, int prezzo, ETipoMoto tipo, int numTempi) : this(marca, modello, tipo, numTempi)
+        {
+            VIN = vin;
+            Colore = colore;
+            km = Km;
+            Alimentazione = alimentazione;
+            maxSpeed = MaxSpeed;
+            Potenza = potenza;
+            dataImmatricolazione = Data;
+            Prezzo = prezzo;
+        }
+        public override string ToString()
+        {
+            return "";
+            /*string stOut = base.ToString();
+            if (Tipo!=ETipoMoto.Undefined) stOut += " Tipo: "+Tipo;
+            if (NumTempi > 0) stOut += " Num.Tempi: " + NumTempi;
+            return stOut;*/
+        }
+        public string ToString(bool isExtended = false)
+        {
+            if (!isExtended) return ToString();
+            else return " ";
+        }
+    }
+}
---
+++ CarShopLibrary/Veicolo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShopLibrary
+{
+    public enum EAlimentazione
+    {
+        Benzina,
+        Diesel,
+        Elettrica,
+        GPL,
+        Metano,
+        FullHybrid,
+        PluginHibrid
+    }
+    public abstract class Veicolo
+    {
+        public string Marca { get; set; }
+        public string Modello { get; set; }
+        public string VIN { get; set; }
+        public string Colore { get; set; }
+        public int km { get; set; }
+        public EAlimentazione Alimentazione { get; set; }
+        public int maxSpeed { get; set; }
+        public int Potenza { get; set; }
+        public DateTime dataImmatricolazione { get; set; } 
+        public int Prezzo { get; set; }
+
+
+        public Veicolo(string marca, string modello)
+        {
+            Marca = marca;
+            Modello = modello;
+        }
+
+        public Veicolo(string marca, string modello, string vin, string colore, int Km, EAlimentazione alimentazione, int MaxSpeed, int potenza, DateTime Data, int prezzo) : this(marca, modello)
+        {
+            VIN = vin;
+            km = Km;
+            maxSpeed = MaxSpeed;
+            Potenza = potenza;
+            dataImmatricolazione = Data;
+            Prezzo = prezzo;
+        }
+
+        public override string ToString()
+        {
+            return Marca+" "+Modello;
+        }
+    }
+}
