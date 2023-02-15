bd8d354dc8ff3e1eb7666dd3825b8e6486531b12
NotoRen <s.noto.2282@vallauri.edu>
2023-02-15 12:44:07 +0100

    update

---
+++ CarShopLibrary/JsonTools.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace CarShopLibrary
+{
+    public class JsonTools
+    {
+        public static void SalvaDati(List<Veicolo> dati)
+        {
+            string jsonString=JsonSerializer.Serialize(dati);
+            Console.WriteLine(jsonString);
+        }
+    }
+}
