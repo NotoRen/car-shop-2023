878230e1d37f11465deffcea9b1d7abfc88474e5
NotoRen <s.noto.2282@vallauri.edu>
2023-02-15 13:01:08 +0100

    update

---
+++ CarShopLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShopLibrary
+{
+    public abstract class Veicolo
+    {
+        public string Marca { get; set; }
+        public string Modello { get; set; }
+
+
+
+
+    }
+}
